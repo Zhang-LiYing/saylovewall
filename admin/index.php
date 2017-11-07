ed66403cad134aef6ea9db1b47a7f6d56026f73f
平兄 <597914752@qq.com>
2017-11-07 08:57:42 +0800

    编辑基本的登录框架

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,17 @@
+<?php
+    session_start();
+    if (isset($_SESSION['login']) && $_SESSION['login'] > 0) {
+        # 如果存在登录记录则进入后台。注意：因为Cookie被微信禁用，导致$_SESSION，所以用微信登录是无法打开后台的。
+        
+    }else {
+        # 使用脚本重定向回到登录界面
+        $url="login.php";
+        echo "<script language=\"javascript\">";
+        echo "location.href=\"$url\"";
+        echo "</script>";
+        exit();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
